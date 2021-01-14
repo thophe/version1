a734e230371dd78a03c73215cbe171be3e7d0647
Christophe Fessard <cfieformation@gmail.com>
2021-01-14 12:22:10 +0100

    Nouvelle version

--- src/main/java/com/ecolepratique/version1/AutreClase3.java
+++ src/main/java/com/ecolepratique/version1/AutreClase3.java
@@ -2,4 +2,8 @@ package com.ecolepratique.version1;
 
 public class AutreClase3 {
 
+	public AutreClase3() {
+		System.out.println("Modif 1");
+	}
+
 }
--- src/main/java/com/ecolepratique/version1/AutreClasse.java
+++ src/main/java/com/ecolepratique/version1/AutreClasse.java
@@ -4,6 +4,7 @@ public class AutreClasse {
 
 	public AutreClasse() {
 		System.out.println("Autre message");
+		System.out.println("Modif 1");
 	}
 
 	
